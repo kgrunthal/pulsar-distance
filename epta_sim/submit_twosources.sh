7abb2d998025159264845a88ab07e7a8dac3a606
Kathrin <k.grunthal@t-online.de>
2024-07-22 16:40:27 +0200

    latest changes

--- epta_sim/submit_twosources.sh
+++ epta_sim/submit_twosources.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sbatch -o=./test_GW_twobins_7-10/out.txt -e=./test_GW_twobins_7-10/err.txt -q short.q --time=00:30:00 --mem=12GB --wrap="singularity exec -B /scratch/,/u/,/hercules/ /scratch/kgrunthal/EPTA_singularity/ python3 ../libstempo_multibin.py"
+FOLDER=test_GW7_RN12
+sbatch -o=./$FOLDER\/out.txt -e=./$FOLDER\/err.txt -p short.q --time=00:30:00 --mem=12GB --wrap="singularity exec -B /scratch/,/u/,/hercules/ /scratch/kgrunthal/EPTA_singularity/ python3 ../libstempo_multibin.py"
 
 
