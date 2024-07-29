fc4e1cf441a8af91bb3bbe16f175a5c6dc965f1e
Kathrin <k.grunthal@t-online.de>
2024-07-29 09:42:34 +0200

    epta results

--- epta_sim/submit_singlesource.sh
+++ epta_sim/submit_singlesource.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FOLDER=test_bin7_A1e-11_2
+FOLDER=test_bin7_A5e-12_2
 sbatch -o ./$FOLDER/out.txt -e ./$FOLDER/err.txt -p short.q --time=00:30:00 --job-name=singlebin --mem=12GB --wrap="singularity exec -B /scratch/,/u/,/hercules/ /scratch/kgrunthal/EPTA_singularity/ python3 ../libstempo_singlebin.py"
 
 
